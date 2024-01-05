56e47bc0b57f43dcf649cd7a23afd50b95aeea3e
mins199600 <mins19960091@gmail.com>
2024-01-05 17:14:41 +0900

    gggg

--- src/com/lotteriaKiosk/hamburgerManager/Member.java
+++ src/com/lotteriaKiosk/hamburgerManager/Member.java
@@ -1,4 +1,14 @@
 package com.lotteriaKiosk.hamburgerManager;
 
 public class Member {
+    public String name;
+    public int number;
+    public int age;
+
+    //생성자
+    public Member(String name, int number, int age){
+        this.name = name;
+        this.number = number;
+        this.age = age;
+    }
 }
